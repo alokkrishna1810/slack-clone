e70c4489a15560421c0702bd7648e827eb03bde0
Alok Krishna <alokkrishna456@gmail.com>
2025-11-03 15:49:34 +0530

    stream & sentry added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "slack-c" });
 
@@ -20,17 +21,25 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    });
   }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id: "delete-user-from-db"},
-    {event: "clerk/user.deleted"},
-    async ({event}) => {
-        await connectDB();
-        const {id} = event.data;
-        await User.deleteOne({clerkId: id})
-    }
-)
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
+  }
+);
 
 export const functions = [syncUser, deleteUserFromDB];
