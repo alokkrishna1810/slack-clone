89814feb32f0286cf62b76bd819ea81cb69a0c0b
Alok Krishna <alokkrishna456@gmail.com>
2025-11-08 19:06:38 +0530

    fixed useLoaction Error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import {
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
